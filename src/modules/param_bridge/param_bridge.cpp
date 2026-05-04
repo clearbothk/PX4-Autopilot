231c364c86b0b0b43370653fae7dbc2b1be93290
Utkarsh Goel <hi@utkarshgoel.dev>
2026-05-05 01:50:24 +0530

    refactor: cleanup Run logic, replace nested if-else for switch, add error logging

--- src/modules/param_bridge/param_bridge.cpp
+++ src/modules/param_bridge/param_bridge.cpp
@@ -1,148 +1,178 @@
+#include <drivers/drv_hrt.h>
+#include <parameters/param.h>
 #include <px4_platform_common/defines.h>
 #include <px4_platform_common/module.h>
 #include <px4_platform_common/module_params.h>
 #include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
-#include <drivers/drv_hrt.h>
-#include <parameters/param.h>
-#include <uORB/Subscription.hpp>
+#include <string.h>
 #include <uORB/Publication.hpp>
+#include <uORB/Subscription.hpp>
 #include <uORB/topics/param_bridge_request.h>
 #include <uORB/topics/param_bridge_response.h>
-#include <string.h>
 
 using namespace time_literals;
 
-class ParamBridge : public ModuleBase<ParamBridge>, public ModuleParams, public px4::ScheduledWorkItem
-{
+class ParamBridge : public ModuleBase<ParamBridge>,
+                    public ModuleParams,
+                    public px4::ScheduledWorkItem {
 public:
-	ParamBridge() : ModuleParams(nullptr), ScheduledWorkItem(MODULE_NAME, px4::wq_configurations::lp_default) {}
-	~ParamBridge() { ScheduleClear(); }
+  ParamBridge()
+      : ModuleParams(nullptr),
+        ScheduledWorkItem(MODULE_NAME, px4::wq_configurations::lp_default) {}
+  ~ParamBridge() { ScheduleClear(); }
 
-	static int task_spawn(int argc, char *argv[]);
-	static int custom_command(int argc, char *argv[]);
-	static int print_usage(const char *reason = nullptr);
+  static int task_spawn(int argc, char *argv[]);
+  static int custom_command(int argc, char *argv[]);
+  static int print_usage(const char *reason = nullptr);
 
-	bool init();
+  void handle_get_request(param_bridge_request_s *req, param_t *p,
+                          param_bridge_response_s *res);
+  void handle_set_request(param_bridge_request_s *req, param_t *p,
+                          param_bridge_response_s *res);
+
+  bool init();
 
 private:
-	void Run() override;
+  void Run() override;
 
-	uORB::Subscription _request_sub{ORB_ID(param_bridge_request)};
-	uORB::Publication<param_bridge_response_s> _response_pub{ORB_ID(param_bridge_response)};
+  uORB::Subscription _request_sub{ORB_ID(param_bridge_request)};
+  uORB::Publication<param_bridge_response_s> _response_pub{
+      ORB_ID(param_bridge_response)};
 };
 
-bool ParamBridge::init()
-{
-	ScheduleOnInterval(10_ms);
-	return true;
+bool ParamBridge::init() {
+  // TODO: Explain why this is really needed?
+  ScheduleOnInterval(10_ms);
+  return true;
 }
 
-void ParamBridge::Run()
-{
-	if (should_exit()) {
-		ScheduleClear();
-		exit_and_cleanup();
-		return;
-	}
+void ParamBridge::handle_get_request(param_bridge_request_s *req, param_t *p,
+                                     param_bridge_response_s *res) {
+  switch (param_type(*p)) {
+  case PARAM_TYPE_INT32:
+    param_get(*p, &(res->int_value));
+    res->param_type = param_bridge_response_s::TYPE_INT;
+    res->status = param_bridge_response_s::STATUS_OK;
+    break;
 
-	param_bridge_request_s req;
+  case PARAM_TYPE_FLOAT:
+    param_get(*p, &(res->float_value));
+    res->param_type = param_bridge_response_s::TYPE_FLOAT;
+    res->status = param_bridge_response_s::STATUS_OK;
+    break;
 
-	while (_request_sub.update(&req)) {
-		param_bridge_response_s rsp{};
-		rsp.timestamp = hrt_absolute_time();
-		memcpy(rsp.param_name, req.param_name, sizeof(rsp.param_name));
-		rsp.param_name[sizeof(rsp.param_name) - 1] = '\0';
-
-		param_t p = param_find_no_notification(req.param_name);
-
-		if (p == PARAM_INVALID) {
-			PX4_WARN("param not found: %.16s", req.param_name);
-			rsp.status = param_bridge_response_s::STATUS_NOT_FOUND;
-
-		} else if (req.operation == param_bridge_request_s::OP_GET) {
-			if (param_type(p) == PARAM_TYPE_INT32) {
-				param_get(p, &rsp.int_value);
-				rsp.param_type = param_bridge_response_s::TYPE_INT;
-				rsp.status = param_bridge_response_s::STATUS_OK;
-
-			} else if (param_type(p) == PARAM_TYPE_FLOAT) {
-				param_get(p, &rsp.float_value);
-				rsp.param_type = param_bridge_response_s::TYPE_FLOAT;
-				rsp.status = param_bridge_response_s::STATUS_OK;
-
-			} else {
-				rsp.status = param_bridge_response_s::STATUS_ERROR;
-			}
-
-		} else if (req.operation == param_bridge_request_s::OP_SET) {
-			int set_ret = -1;
-
-			if (param_type(p) == PARAM_TYPE_INT32) {
-				set_ret = param_set(p, &req.int_value);
-
-			} else if (param_type(p) == PARAM_TYPE_FLOAT) {
-				set_ret = param_set(p, &req.float_value);
-			}
-
-			if (set_ret == 0) {
-				param_save_default(false);
-				rsp.status = param_bridge_response_s::STATUS_OK;
-
-			} else {
-				PX4_ERR("SET %.16s failed (%d)", req.param_name, set_ret);
-				rsp.status = param_bridge_response_s::STATUS_ERROR;
-			}
-
-		} else {
-			rsp.status = param_bridge_response_s::STATUS_ERROR;
-		}
-
-		_response_pub.publish(rsp);
-	}
+  default:
+    PX4_ERR("Unsupported param type for GET: %d on param %.16s", param_type(*p),
+            req->param_name);
+    res->status = param_bridge_response_s::STATUS_ERROR;
+  }
 }
 
-int ParamBridge::task_spawn(int argc, char *argv[])
-{
-	ParamBridge *instance = new ParamBridge();
+void ParamBridge::handle_set_request(param_bridge_request_s *req, param_t *p,
+                                     param_bridge_response_s *res) {
+  switch (param_type(*p)) {
+  case PARAM_TYPE_INT32:
+    if (param_set(*p, &(req->int_value)) == 0) {
+      param_save_default(false);
+      res->int_value = req->int_value;
+      res->param_type = param_bridge_response_s::TYPE_INT;
+      res->status = param_bridge_response_s::STATUS_OK;
+    } else {
+      PX4_ERR("SET %.16s failed", req->param_name);
+      res->status = param_bridge_response_s::STATUS_ERROR;
+    }
+    break;
 
-	if (instance) {
-		_object.store(instance);
-		_task_id = task_id_is_work_queue;
+  case PARAM_TYPE_FLOAT:
+    if (param_set(*p, &(req->float_value)) == 0) {
+      param_save_default(false);
+      res->float_value = req->float_value;
+      res->param_type = param_bridge_response_s::TYPE_FLOAT;
+      res->status = param_bridge_response_s::STATUS_OK;
+    } else {
+      PX4_ERR("SET %.16s failed", req->param_name);
+      res->status = param_bridge_response_s::STATUS_ERROR;
+    }
+    break;
 
-		if (instance->init()) {
-			return PX4_OK;
-		}
-
-	} else {
-		PX4_ERR("alloc failed");
-	}
-
-	delete instance;
-	_object.store(nullptr);
-	_task_id = -1;
-
-	return PX4_ERROR;
+  default:
+    PX4_ERR("Unsupported param type for SET: %d on param %.16s", param_type(*p),
+            req->param_name);
+    res->status = param_bridge_response_s::STATUS_ERROR;
+  }
 }
 
-int ParamBridge::custom_command(int argc, char *argv[])
-{
-	return print_usage("unknown command");
+void ParamBridge::Run() {
+  if (should_exit()) {
+    ScheduleClear();
+    exit_and_cleanup();
+    return;
+  }
+
+  param_bridge_request_s req;
+  param_bridge_response_s res{};
+
+  while (_request_sub.update(&req)) {
+    res.timestamp = hrt_absolute_time();
+    memcpy(res.param_name, req.param_name, sizeof(res.param_name));
+    res.param_name[sizeof(res.param_name) - 1] = '\0';
+
+    param_t p = param_find_no_notification(req.param_name);
+
+    if (p == PARAM_INVALID) {
+      PX4_WARN("param not found: %.16s", req.param_name);
+      res.status = param_bridge_response_s::STATUS_NOT_FOUND;
+    } else if (req.operation == param_bridge_request_s::OP_GET) {
+      handle_get_request(&req, &p, &res);
+    } else if (req.operation == param_bridge_request_s::OP_SET) {
+      handle_set_request(&req, &p, &res);
+    } else {
+      res.status = param_bridge_response_s::STATUS_ERROR;
+    }
+
+    _response_pub.publish(res);
+  }
 }
 
-int ParamBridge::print_usage(const char *reason)
-{
-	if (reason) {
-		PX4_WARN("%s\n", reason);
-	}
+int ParamBridge::task_spawn(int argc, char *argv[]) {
+  ParamBridge *instance = new ParamBridge();
 
-	PRINT_MODULE_DESCRIPTION("Parameter bridge: GET/SET PX4 params by name over uXRCE-DDS");
-	PRINT_MODULE_USAGE_NAME("param_bridge", "system");
-	PRINT_MODULE_USAGE_COMMAND("start");
-	PRINT_MODULE_USAGE_DEFAULT_COMMANDS();
-	return 0;
+  if (instance) {
+    _object.store(instance);
+    _task_id = task_id_is_work_queue;
+
+    if (instance->init()) {
+      return PX4_OK;
+    }
+
+  } else {
+    PX4_ERR("alloc failed");
+  }
+
+  delete instance;
+  _object.store(nullptr);
+  _task_id = -1;
+
+  return PX4_ERROR;
 }
 
-extern "C" __EXPORT int param_bridge_main(int argc, char *argv[])
-{
-	return ParamBridge::main(argc, argv);
+int ParamBridge::custom_command(int argc, char *argv[]) {
+  return print_usage("unknown command");
+}
+
+int ParamBridge::print_usage(const char *reason) {
+  if (reason) {
+    PX4_WARN("%s\n", reason);
+  }
+
+  PRINT_MODULE_DESCRIPTION(
+      "Parameter bridge: GET/SET PX4 params by name over uXRCE-DDS");
+  PRINT_MODULE_USAGE_NAME("param_bridge", "system");
+  PRINT_MODULE_USAGE_COMMAND("start");
+  PRINT_MODULE_USAGE_DEFAULT_COMMANDS();
+  return 0;
+}
+
+extern "C" __EXPORT int param_bridge_main(int argc, char *argv[]) {
+  return ParamBridge::main(argc, argv);
 }
